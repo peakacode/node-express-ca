43019d1b83d89d0baaf0f86912f3ff310df4925b
splinter <you@example.com>
2022-09-29 20:17:47 +0800

    HTTP Request Object

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'content-type': 'text/html'})
-  res.write('<h1>Home Page</h1>')
-  res.end()
+  // console.log(req.method) // GET
+  console.log(req.url) // i.e. /Contact
+  const url = req.url;
+  // home page
+  if (url === '/') {
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>Home Page</h1>')
+    res.end()
+  // about page
+  } else if (req.url === '/about') {
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>About Page</h1>')
+    res.end()
+  // 404 page 
+  } else {
+    res.writeHead(404, {'content-type': 'text/html'})
+    res.write('<h1>404 Page</h1>')
+    res.end()
+  }
 })
 
 server.listen(5000)
