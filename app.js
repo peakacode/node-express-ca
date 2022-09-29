6cd88b3e42dfc2812659d569c3a89f1d1f67648a
splinter <you@example.com>
2022-09-29 20:31:59 +0800

    HTTP - HTML File

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const http = require('http')
+const { readFileSync } = require('fs')
+
+// get all files
+const homePage = readFileSync('./index.html')
+const aboutPage = readFileSync('./about.html')
+const pageNotFound = readFileSync('./404.html')
 
 const server = http.createServer((req, res) => {
   // console.log(req.method) // GET
-  console.log(req.url) // i.e. /Contact
+  // console.log(req.url) // i.e. /Contact
   const url = req.url;
   // home page
   if (url === '/') {
     res.writeHead(200, {'content-type': 'text/html'})
-    res.write('<h1>Home Page</h1>')
+    res.write(homePage)
     res.end()
   // about page
   } else if (req.url === '/about') {
     res.writeHead(200, {'content-type': 'text/html'})
-    res.write('<h1>About Page</h1>')
+    res.write(aboutPage)
     res.end()
   // 404 page 
   } else {
     res.writeHead(404, {'content-type': 'text/html'})
-    res.write('<h1>404 Page</h1>')
+    res.write(pageNotFound)
     res.end()
   }
 })
