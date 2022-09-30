a7efb0b70569b8c01285e671c5bf3abfcbe29b8c
splinter <you@example.com>
2022-09-30 16:36:39 +0800

    Query String

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { products } = require('./data')
+
+// // app.use - to setup static and middleware
+// app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-  res.status(200).send('Home Page')
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
 })
 
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const {id, name, image} = product;
+    return {id, name, image}
+  })
+  res.json(newProducts)
 })
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>404 error.</h1> Resource not found')
+app.get('/api/products/:productID', (req, res) => {
+  // console.log(req)
+  // console.log(req.params)
+  const { id } = req.params;
+
+  const singleProduct = products.find((product) => product.id === Number(productID))
+  if (!singleProduct) {
+    return res.status(404).send('Product Does Not Exist')
+  }
+  console.log(singleProduct)
+  res.json(singleProduct)
 })
 
-const PORT = 5000
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params)
+  res.send('Hello World')
+})
+
+
+app.get('/api/v1/query', (req, res) => {
+  // console.log(req.query)
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  if (sortedProducts.length < 1) {
+    // option 1
+    // res.status(200).send('no product matches your search');
+    return res.status(200).json({ success: true, data: []})
+  }
+  res.status(200).json(sortedProducts)
+})
+
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
 })
-
-// app.get = Read Data
-// app.post = Insert Data
-// app.put = Update Data
-// app.delete = Delete Data
-// app.all = Get all Data
-// app.use
-// app.listen
-
-
